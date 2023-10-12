6c06f9f6388e77d6895c0723ea3f84559ac3e758
Jaime Pacheco <59517762+JaimP07@users.noreply.github.com>
2023-10-12 03:26:55 -0700

    Add files via upload

---
+++ gennoise.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random as random
+
+from scipy.linalg import svdvals
+from sklearn.preprocessing import normalize
+
+#Generates noise of a given mean and standard deviation for a linear system Ax=b.
+#Returns the result of b shifted by noise, as well as the maximum element in the noise vector
+def generateNoise(b,mu,sig):
+  m_b = b.shape[0]
+  noise = 1*np.random.normal(mu, sig, size=(m_b,1))
+  noise_max = noise.max()
+  return [b + noise, noise_max]
